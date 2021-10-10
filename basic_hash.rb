17f52c7880788092fe25ed5d76f42c86eaaa027d
MEG HOWE <howem9@gmail.com>
2021-10-10 19:16:32 -0400

    adds menu hash problem solution

--- basic_hash.rb
+++ basic_hash.rb
@@ -90,3 +90,36 @@ end
 count_votes(["Dewey", "Truman", "Dewey", "Dewey", "Truman", "Truman", "Dewey", "Truman", "Truman", "Dewey", "Dewey"])
 
 #============================================================================
+
+# 3. Order the Whole Menu
+
+# Given a hash, where the keys are strings representing food items, and the values are numbers representing the price of each food, return the amount someone would pay if they'd order one of each food from the entire menu.
+
+# Input: {"hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2}
+
+# Output: 14
+
+# Explanation: If someone would order one of everything on the menu, they'd pay a total of 14 (the sum of all the hash's values).
+
+=begin
+  create index
+  create sum
+  total = 0
+
+  hash
+
+  loop through length
+  return 
+  call function
+=end
+
+def menu(hash)
+  index = 0
+  total = 0
+  hash.each do |k, v|
+    total = total + "#{v}".to_i
+  end
+  return total
+end
+
+p menu({ "hot dog" => 2, "hamburger" => 3, "steak sandwich" => 5, "fries" => 1, "cole slaw" => 1, "soda" => 2 })
