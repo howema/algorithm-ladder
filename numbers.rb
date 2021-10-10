bcad9a649659fef4eb7b90dbe0fe8dae3fe381f5
MEG HOWE <howem9@gmail.com>
2021-10-10 15:32:44 -0400

    adds leap year solution

--- numbers.rb
+++ numbers.rb
@@ -153,3 +153,54 @@ end
 p fib(8)
 
 #============================================================================
+
+# 4. Leap Year
+
+# Given a year, report if it is a leap year.
+
+# The tricky thing here is that a leap year in the Gregorian calendar occurs:
+
+# on every year that is evenly divisible by 4
+# except every year that is evenly divisible by 100
+# unless the year is also evenly divisible by 400
+# For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+
+# If your language provides a method in the standard library that does this look-up, pretend it doesn't exist and implement it yourself.
+
+=begin
+
+
+if n % 4 == 0 && n % 100 == 0
+  if n % 400 == 0
+    return true
+  end
+  return false
+end
+
+
+if n % 4 == 0
+  true
+
+=end
+
+def leap_year(n)
+  if n % 4 == 0 && n % 100 == 0
+    if n % 400 == 0
+      return true
+    end
+    return false
+  end
+
+  if n % 4 == 0
+    return true
+  else
+    return false
+  end
+end
+
+p leap_year(1997);  #false
+p leap_year(1996);  #true
+p leap_year(1900);  #false
+p leap_year(2000) #true
+
+#note--1997 printed 'nil' originally (bc excluded return false from bottom loop), run through all edge cases when looping through code/debuggin step
--- whiteboard.rb
+++ whiteboard.rb
@@ -1,66 +1,50 @@
-# 2.Fizzbuzz
+# 4. Leap Year
 
-# Write a function that prints out every number from 1 to N, with the following exceptions:
+# Given a year, report if it is a leap year.
 
-# If the number is divisible by 3, print out "FIZZ".
-# If the number is divisible by 5, print out "BUZZ".
-# If the number is divisible by both 3 and 5, print out "FIZZBUZZ".
+# The tricky thing here is that a leap year in the Gregorian calendar occurs:
+
+# on every year that is evenly divisible by 4
+# except every year that is evenly divisible by 100
+# unless the year is also evenly divisible by 400
+# For example, 1997 is not a leap year, but 1996 is. 1900 is not a leap year, but 2000 is.
+
+# If your language provides a method in the standard library that does this look-up, pretend it doesn't exist and implement it yourself.
 
 =begin
-9 => FIZZ
-5 => BUZZ
-15 => FIZZBUZ
-4 => 4
 
-write a function, add params
-call/print function
 
-create index, += unil reaches N
-while i < n
-  if i % 3 && 5 == 0
-    print fizzbuzz
-  elsif i % 3 == 0
-    print fizz
-  elsif i % 5 == 0
-    print buzz
-  else
-    print i
+if n % 4 == 0 && n % 100 == 0
+  if n % 400 == 0
+    return true
   end
-  index++
+  return false
 end
+
+
+if n % 4 == 0
+  true
 
 =end
 
-def fizzbuzz(n)
-  i = 1
-  while i <= n
-    if i % 3 == 0 && i % 5 == 0
-      puts "FIZZBUZZ"
-    elsif i % 3 == 0
-      puts "FIZZ"
-    elsif i % 5 == 0
-      puts "BUZZ"
-    else
-      puts i
+def leap_year(n)
+  if n % 4 == 0 && n % 100 == 0
+    if n % 400 == 0
+      return true
     end
-    i += 1
+    return false
+  end
+
+  if n % 4 == 0
+    return true
+  else
+    return false
   end
 end
 
-fizzbuzz(15)
-# will print:
-#1
-#2
-#FIZZ
-#4
-#BUZZ
-#FIZZ
-#7
-#8
-#FIZZ
-#BUZZ
-#11
-#FIZZ
-#13
-#14
-#FIZZBUZZ
+p leap_year(1997);  #false
+p leap_year(1996);  #true
+p leap_year(1900);  #false
+p leap_year(2000) #true
+
+#note--1997 printed 'nil' originally (bc excluded return false from bottom loop), run through all edge cases when looping through code/debuggin step
