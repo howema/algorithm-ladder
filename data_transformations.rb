fc8bc0be92c0d455c6090640fa6b1eae3b36d013
MEG HOWE <howem9@gmail.com>
2021-10-19 14:49:28 -0400

    adds flip and flatten solution

--- data_transformations.rb
+++ data_transformations.rb
@@ -138,3 +138,137 @@ p flip_hash({ "a" => 1, "b" => 2, "c" => 3 })
 #assigment. just the inverse of new_hash[k] = v
 
 #============================================================================
+
+# 4. ETL 2      /Extract, Transform, Load
+
+# You are given a hash in format #A, and you are to return the same data as a hash using format #B, as specified below:
+
+# Input:
+
+# {
+# 1 => ["A", "E", "I", "O", "U"]
+# }
+
+# Output:
+
+# {
+# 'a' => 1,
+# 'e' => 1,
+# 'i' => 1,
+# 'o' => 1,
+# 'u' => 1
+# }
+
+# Input:
+
+# {
+# 1 => ["A", "E"],
+# 2 => ["D", "G"]
+# }
+
+# Output:
+
+# {
+# 'a' => 1,
+# 'd' => 2,
+# 'e' => 1,
+# 'g' => 2
+# }
+
+# Input:
+
+# {
+# 1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+# 2 => ["D", "G"],
+# 3 => ["B", "C", "M", "P"],
+# 4 => ["F", "H", "V", "W", "Y"],
+# 5 => ["K"],
+# 8 => ["J", "X"],
+# 10 => ["Q", "Z"]
+# }
+
+# Output:
+
+# {
+# 'a' => 1,
+# 'b' => 3,
+# 'c' => 3,
+# 'd' => 2,
+# 'e' => 1,
+# 'f' => 4,
+# 'g' => 2,
+# 'h' => 4,
+# 'i' => 1,
+# 'j' => 8,
+# 'k' => 5,
+# 'l' => 1,
+# 'm' => 3,
+# 'n' => 1,
+# 'o' => 1,
+# 'p' => 3,
+# 'q' => 10,
+# 'r' => 1,
+# 's' => 1,
+# 't' => 1,
+# 'u' => 1,
+# 'v' => 4,
+# 'w' => 4,
+# 'x' => 8,
+# 'y' => 4,
+# 'z' => 10
+# }
+
+=begin
+taking in a hash that's a number as key and array as value, but returning a hash that has each element of the array as a string with the value as the number
+
+flipping
+downcase
+
+create empty hash
+
+each loop- only option- |k, v|
+  for every v
+
+    each loop for array |element|
+      for every element in the hash[value]
+        new_hash[k] = array[element].downcase
+        new_hash[v] = k
+
+return new hash
+function, params, call function
+=end
+
+def transform(hash)
+  new_hash = {}
+  hash.each do |k, v|
+    v.each do |element|
+      new_hash[element.downcase] = k
+    end
+  end
+
+  new_hash = Hash[new_hash.sort_by { |k, v| k.to_s }]
+  #makes alphabetical
+
+  return new_hash
+end
+
+p transform({ 1 => ["A", "E", "I", "O", "U"] })
+p transform({
+  1 => ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+  2 => ["D", "G"],
+  3 => ["B", "C", "M", "P"],
+  4 => ["F", "H", "V", "W", "Y"],
+  5 => ["K"],
+  8 => ["J", "X"],
+  10 => ["Q", "Z"],
+})
+
+# Output:
+
+# {
+# 'a' => 1,
+# 'e' => 1,
+# 'i' => 1,
+# 'o' => 1,
+# 'u' => 1
+# }
