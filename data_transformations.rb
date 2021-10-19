15474a741bccf9eb91fff6f0d380e85dce6194cf
MEG HOWE <howem9@gmail.com>
2021-10-19 14:00:01 -0400

    adds flip hash solution

--- data_transformations.rb
+++ data_transformations.rb
@@ -101,3 +101,40 @@ p flatten_hash({ "a" => 1, "b" => 2, "c" => 3, "d" => 4 })
 # Output: ["a", 1, "b", 2, "c", 3, "d", 4]
 
 #============================================================================
+
+# 3. Flip Hash
+
+# Given a hash, create a new hash that has the keys and values switched.
+
+# Input: {"a" => 1, "b" => 2, "c" => 3}
+# Output: {1 => "a", 2 => "b", 3 => "c"}
+
+=begin
+create new_hash as empty
+
+each loop because INPUT is hash
+  have brackets
+  maybe use puts to understand values
+
+  for every element in the hash, new_hash = og_hash[value], THEN the key
+    use assignment, not shovel
+    may need 2 assignments per loop?===> NO
+return new hash
+
+function, params are og hash, call funtion
+=end
+
+def flip_hash(og_hash)
+  new_hash = {}
+  og_hash.each do |k, v|
+    new_hash[v] = k
+  end
+  return new_hash
+end
+
+p flip_hash({ "a" => 1, "b" => 2, "c" => 3 })
+# Output: {1 => "a", 2 => "b", 3 => "c"}
+
+#assigment. just the inverse of new_hash[k] = v
+
+#============================================================================
--- whiteboard.rb
+++ whiteboard.rb
@@ -1,29 +1,34 @@
-# 2. Flatten Hash
+# 3. Flip Hash
 
-# Given a hash, return a flat array containing all the hash’s keys and values.
+# Given a hash, create a new hash that has the keys and values switched.
 
-# Input: {"a" => 1, "b" => 2, "c" => 3, "d" => 4}
-# Output: ["a", 1, "b", 2, "c", 3, "d", 4]
+# Input: {"a" => 1, "b" => 2, "c" => 3}
+# Output: {1 => "a", 2 => "b", 3 => "c"}
 
 =begin
-create empty array
-each loop
+create new_hash as empty
 
-for every k,v pair in the hash, push the key and then the value to the array
+each loop because INPUT is hash
+  have brackets
+  maybe use puts to understand values
 
-return array
+  for every element in the hash, new_hash = og_hash[value], THEN the key
+    use assignment, not shovel
+    may need 2 assignments per loop?===> NO
+return new hash
 
-function, params, call function
+function, params are og hash, call funtion
 =end
 
-def flatten_hash(hash)
-  array = []
-  hash.each do |k, v|
-    array << k
-    array << v
+def flip_hash(og_hash)
+  new_hash = {}
+  og_hash.each do |k, v|
+    new_hash[v] = k
   end
-  return array
+  return new_hash
 end
 
-p flatten_hash({ "a" => 1, "b" => 2, "c" => 3, "d" => 4 })
-# Output: ["a", 1, "b", 2, "c", 3, "d", 4]
+p flip_hash({ "a" => 1, "b" => 2, "c" => 3 })
+# Output: {1 => "a", 2 => "b", 3 => "c"}
+
+#assigment. just the inverse of new_hash[k] = v
